36eb8ed866861d6918b8505aeab464733f22413a
amitdadwal <amitdadwal.mae.engg@gmail.com>
2025-04-12 21:20:41 +0530

    code up

---
+++ src/redux/store/store.js
@@ -0,0 +1,12 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "@redux/slices/authSlice";
+// import productReducer from "@redux/slices/productSlice";
+
+// const store = configureStore({
+//     reducer: {
+//       auth: authReducer,
+//       product: productReducer
+//     },
+//   });
+
+// export default store;
