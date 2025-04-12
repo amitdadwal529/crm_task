2405cec2691ce8969203a831d40abf445dda35fd
amitdadwal <amitdadwal.mae.engg@gmail.com>
2025-04-12 22:54:42 +0530

    redirect to login

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "@redux/slices/authSlice"
+import productReducer from "@redux/slices/productSlice"
 
 const store = configureStore({
     reducer: {
-    //   auth: authReducer,
-    //   product: productReducer
+      auth: authReducer,
+      product: productReducer
     },
   });
 
