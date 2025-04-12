91a62365a2937199230ce34eb991a533a5df2b46
amitdadwal <amitdadwal.mae.engg@gmail.com>
2025-04-12 21:17:00 +0530

    made productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -8,15 +8,14 @@ const getAllProducts = async () => {
 
 
 
-const addProduct = async (data) => {
-   
+const addProduct = async (data) => {  
     const response = await handleRequest("POST", PRODUCTS.ADD, data);
     return response;
 };
 
-const editProduct = async (data) => {
+const updateProduct = async (data) => {
    
-    const response = await handleRequest("POST", EDIT_PRODUCT, data);
+    const response = await handleRequest("POST", PRODUCTS.UPDATE, data);
     return response;
 };
 
@@ -24,37 +23,24 @@ const editProduct = async (data) => {
 const deleteProduct = async (id) => {
   
    
-    const response = await handleRequest("DELETE", DELETE_PRODUCT+"/"+id);
+    const response = await handleRequest("DELETE", PRODUCTS.DELETE+"/"+id);
     return response;
 };
 
-const getProductDetails = async (id) => {
+const getProductDetail = async (id) => {
    
-    const response = await handleRequest("GET", GET_PRODUCT_DETAILS+"/"+id);
+    const response = await handleRequest("GET", PRODUCTS.GET_SINGLE+"/"+id);
     return response;
 };
 
 const deleteMultipleProducts = async (ids) => {
    
-    const response = await handleRequest("DELETE", DELETE_BULK_PRODUCT+"?ids="+ids);
+    const response = await handleRequest("DELETE", PRODUCTS.GET_ALL+"?ids="+ids);
     return response;
 };
 
-const  getPrductGallery=async(url="?offset=1&limit=50")=>{
-    const response =  await handleRequest("GET",GET_PRODUCT_GALLERY+url)
-  return response
-}
-
-const uploadGallery = async(data)=>{
-    const response =  await handleRequest("POST",UPLOAD_GALLERY,data)
-    return response
-}
-
-const bulkProductUpload = async(data)=>{
-    const response =  await handleRequest("POST",BULK_PRODUCT_UPLOAD,data,true)
-    return response
-}
 
 
 
-export default { getProductList ,createProduct,deleteProduct,getProductDetails,deleteMultipleProducts,editProduct,getPrductGallery,uploadGallery,bulkProductUpload,getCategoryList}
+
+export default {getAllProducts, addProduct, updateProduct, deleteProduct, getProductDetail, deleteMultipleProducts}
