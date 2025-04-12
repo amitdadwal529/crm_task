23d3b8c3ccdb1801e84a1846dc1d3e6849d9e0c1
amitdadwal <amitdadwal.mae.engg@gmail.com>
2025-04-12 23:57:19 +0530

    side path added

--- src/components/ui/sidebar/CustomSidebar.jsx
+++ src/components/ui/sidebar/CustomSidebar.jsx
@@ -1,11 +1,44 @@
 import React, { useState } from 'react';
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { SidebarHeader } from '@components/ui/sidebar/SidebarHeader';
 import { FaTachometerAlt, FaBoxOpen } from 'react-icons/fa';
+import { PRIVATE_ROUTES } from '@routes/routes';
+import { MdDashboard } from "react-icons/md";
+import { BsBoxFill } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import { GoDotFill } from "react-icons/go";
 
 export const CustomSidebar = (props) => {
   const { setBroken, toggled, setToggled } = props;
   const [collapsed, setCollapsed] = useState(false);
+
+  const menuItems = [
+    {
+      label: 'Dashboard',
+      icon: <MdDashboard className="text-red-500  text-lg" />,
+      path: PRIVATE_ROUTES.DASHBOARD,
+      subMenu: []
+    },
+   
+    {
+      label: 'Product Management',
+      icon: <BsBoxFill className="text-red-500" />,
+      path: '',
+      subMenu: [
+        { label: 'Products', path: PRIVATE_ROUTES.PRODUCTS},
+        { label: 'Add Product', path: PRIVATE_ROUTES.ADD_PRODUCT },
+      ]
+    },
+
+  ];
+
+  const isActive = (path) => location.pathname === path ? 'bg-gray-200' : ''; // Tailwind active color
+ 
+  
+   // Function to check if any submenu under the current main menu is active
+   const isSubMenuActive = (subMenu) => {
+    return subMenu.some(item => location.pathname === item.path) ? 'bg-gray-200' : '';
+  };
 
   return (
     <div style={{ display: 'flex', height: '100%' }}>
@@ -30,8 +63,42 @@ export const CustomSidebar = (props) => {
               label: { fontWeight: 500 },
             }}
           >
-            <MenuItem icon={<FaTachometerAlt />}>Dashboard</MenuItem>
-            <MenuItem icon={<FaBoxOpen />}>Product Management</MenuItem>
+           {menuItems.map((menuItem, index) => (
+      menuItem.subMenu.length === 0 ? (
+        <Link
+          key={index}
+          to={menuItem.path}
+        
+        >
+          <MenuItem className={isActive(menuItem.path)}>
+            <p className="flex items-center gap-3 ml-2">
+              {menuItem.icon} {menuItem.label}
+            </p>
+          </MenuItem>
+        </Link>
+      ) : (
+        <SubMenu
+          key={index}
+          label={menuItem.label}
+          icon={menuItem.icon}
+          className={isActive(menuItem.path) || isSubMenuActive(menuItem.subMenu)}
+        >
+          {menuItem.subMenu.map((subMenuItem, subIndex) => (
+            <Link
+              key={subIndex}
+              to={subMenuItem.path}
+             
+            >
+              <MenuItem className={isActive(subMenuItem.path)}>
+                <p className="flex items-center gap-1">
+                  <GoDotFill className="text-red-500 mx-2" /> {subMenuItem.label}
+                </p>
+              </MenuItem>
+            </Link>
+          ))}
+        </SubMenu>
+      )
+    ))}
           </Menu>
         </div>
       </Sidebar>
