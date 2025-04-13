f90f9ca153373de982f44df23e0ebce725f20853
amitdadwal <amitdadwal.mae.engg@gmail.com>
2025-04-13 23:40:19 +0530

    reade me written

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # crm_task
-Customer Relationship Management (CRM) system built using React Js , Redux Toolkit and dummy json apis
+Customer Relationship Management (CRM) system.
+
+Features
+1. Login Page
+Users can log in with a username and password.
+
+Validation checks for correct credentials.
+
+On successful login, users are redirected to the dashboard.
+
+2. Authentication
+Public and private routes implemented using React Router.
+
+Authenticated users can access the dashboard and product management features.
+
+Unauthenticated users are redirected to the login page.
+
+3. Dashboard
+Displays graphs with dummy data using Chart.js and React react-chartjs-2.
+
+Graphs for tracking Sales Overview, Customer Impressions, and Traffic Sources.
+
+
+4. Product Management (CRUD)
+Create: Users can add new products with name, description, and price.
+
+Read: Users can view a list of all products.
+
+Update: I have implemented the dummy update product api succesfully but as we can't change thier server so you wont see any changes  
+
+Delete: I have implemented the dummy delete product api succesfully but as we can't change thier server so you wont see any changes  
+
+Uses Redux Toolkit to manage product data state.
+
+Tech Stack
+Frontend:
+
+React.js: Main library for building the user interface.
+
+Redux: For state management across the app.
+
+Redux Toolkit: For a more efficient Redux implementation.
+
+React Router: For handling navigation and route management.
+
+Chart.js: For implementing graphs and visualizations.
+
+Styling:
+
+Tailwind CSS: For utility-first CSS styling.
+
+React-icons : for icons
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,41 +1,55 @@
 import React, { useState } from 'react'
-import * as Yup from "yup";
+
+// yup for validation
+import * as Yup from "yup"; 
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import { useDispatch } from 'react-redux';
+
+// useForm from React Hook Form for form
+import { useForm } from "react-hook-form"; 
+
+  // Import useDispatch for dispatching Redux actions
+import { useDispatch, useSelector } from 'react-redux'; 
+
+// login thunk action for authentication
 import { login } from '@redux/thunk/authThunk';
-import { FaRegEyeSlash } from 'react-icons/fa6';
+// react icons
+import { FaRegEyeSlash } from 'react-icons/fa6'; 
 import { MdOutlineRemoveRedEye } from 'react-icons/md';
+   // UI component for form inputs
 import Input from '@components/ui/form/Input';
 
+// Define the validation schema 
 const validationSchema = Yup.object({
   username: Yup.string()
     .required("Username is required")
     .min(3, "Username must be at least 3 characters")
-    .matches(/^[a-zA-Z0-9_]+$/, "Only letters, numbers, and underscores are allowed"),
+    .matches(/^[a-zA-Z0-9_]+$/, "Only letters, numbers, and underscores are allowed"), 
 
   password: Yup.string()
-    .required("Password is required")
+    .required("Password is required") 
 });
 
 const Login = () => {
   const {
-    register,
+    register, 
     handleSubmit,
-    formState: { errors },
+    formState: { errors }, 
   } = useForm({
-    resolver: yupResolver(validationSchema),
+    resolver: yupResolver(validationSchema), // Yup validation schema 
   });
 
-  const [showPassword, setShowPassword] = useState(false);
-  const dispatch = useDispatch();
-
+  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+  const dispatch = useDispatch(); // Get the dispatch function from Redux
+  // Handle form submission
   const onSubmit = (data) => {
-    dispatch(login(data));
+    dispatch(login(data)); // Dispatch login action with form data (username and password)
   }
+const {loading} = useSelector((state)=>state.auth)
+  // Toggle password visibility
   const handleToggle = () => {
-    setShowPassword(!showPassword);
+    setShowPassword(!showPassword); // Toggle the password visibility state
   };
+
   return (
     <>
       <section className="bg-gray-100 ">
@@ -45,36 +59,43 @@ const Login = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                 Customer Relation Management
               </h1>
+              {/* Login form */}
               <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 md:space-y-6" action="#">
+                {/* Input for username */}
                 <Input
                   label="Username"
                   name="username"
                   type="text"
                   placeholder="Enter your username"
-                  register={register}
-                  validation={{ required: "Username is required" }}
-                  errors={errors}
+                  register={register} // Register input with react-hook-form
+                  validation={{ required: "Username is required" }} // Validation for username
+                  errors={errors} // Display form errors if any
                   className='border-0'
                 />
                 <div className='relative'>
+                  {/* Input for password */}
                   <Input
                     label="Password"
                     name="password"
-                    type={showPassword ? "text" : "password"}
+                    type={showPassword ? "text" : "password"} // Toggle between text and password type
                     placeholder="••••••••"
-                    register={register}
-                    validation={{ required: "Password is required", minLength: { value: 6, message: "Min 6 characters" } }}
-                    errors={errors}
+                    register={register} // Register input with react-hook-form
+                    validation={{ required: "Password is required", minLength: { value: 6, message: "Min 6 characters" } }} // Password validation
+                    errors={errors} // Display form errors if any
                     className='border-0'
                   />
+                  {/* Toggle icon to show/hide password */}
                   <div className="absolute top-9 right-3" onClick={handleToggle}>
                     {showPassword ?
-                      (<FaRegEyeSlash fontSize={18} />) :
-                      (<MdOutlineRemoveRedEye fontSize={18} />)
+                      (<FaRegEyeSlash fontSize={18} />) : // Show eye-slash icon when password is visible
+                      (<MdOutlineRemoveRedEye fontSize={18} />) // Show eye icon when password is hidden
                     }
                   </div>
                 </div>
-                <button type="submit" className="bg-blue-800 ms-auto text-white hover:bg-blue-950 rounded-xl w-1/2 shadow-xl px-3 py-1">Submit</button>
+                {/* Submit button */}
+                <button disabled={loading} type="submit" className="bg-blue-800 ms-auto text-white hover:bg-blue-950 rounded-xl w-1/2 shadow-xl px-3 py-1">
+                  Submit
+                </button>
               </form>
             </div>
           </div>
@@ -84,4 +105,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
